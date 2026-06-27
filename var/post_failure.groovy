2c2154b351b6ced3629d5a02547910789045649f
shashankcodes-10 <124920174+shashankcodes-10@users.noreply.github.com>
2026-06-27 20:50:26 +0530

    Add failure function to send email on build failure

---
+++ var/post_failure.groovy
@@ -0,0 +1,6 @@
+def failure(){
+                    emailext from: "u10shashank@gmail.com",
+                    to : "u10shashank@gmail.com",
+                    body: "Build failed for ci",
+                    subject : "Build failed for ci"
+}
