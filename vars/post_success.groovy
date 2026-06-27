69d94685115bf61ac7026e350631c76db086b3e3
shashankcodes-10 <124920174+shashankcodes-10@users.noreply.github.com>
2026-06-27 23:22:25 +0530

    Update post_success.groovy

--- vars/post_success.groovy
+++ vars/post_success.groovy
@@ -1,7 +1,9 @@
-def success(){
+def notifySuccess(){
   
-                    emailext from: "u10shashank@gmail.com",
+                    emailext(
+                    from: "u10shashank@gmail.com",
                     to : "u10shashank@gmail.com",
                     body: "Build successfull for ci",
                     subject : "Build successfull for ci"
+                   )   
 }
