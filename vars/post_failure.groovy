8c0cbc1e53d90f9f2047ebba837f8bbda7325e00
shashankcodes-10 <124920174+shashankcodes-10@users.noreply.github.com>
2026-06-27 23:21:39 +0530

    Update post_failure.groovy

--- vars/post_failure.groovy
+++ vars/post_failure.groovy
@@ -1,6 +1,8 @@
 def failure(){
-                    emailext from: "u10shashank@gmail.com",
+                    emailext(
+                    from: "u10shashank@gmail.com",
                     to : "u10shashank@gmail.com",
                     body: "Build failed for ci",
                     subject : "Build failed for ci"
+                )              
 }
