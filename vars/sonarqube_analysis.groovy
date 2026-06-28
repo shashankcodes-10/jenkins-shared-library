769be5a38b8b2c9753a08c2f9a37e14b684d6bdd
shashankcodes-10 <124920174+shashankcodes-10@users.noreply.github.com>
2026-06-28 20:42:27 +0530

    Fix sonar analysis function and update command

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -1,5 +1,5 @@
-def call(String sonarInstallation , String projectName , string projectKey){
-  withsonarQubeEnv(sonarInstallaton){
-          sh " $SONARHOME/bin/sonar-server -Dsonar.projectName:${projectName} -Dsonar.projectKey:${projectKey}
-  }
+def call(String sonarInstallation, String projectName, String projectKey) {
+    withSonarQubeEnv(sonarInstallation) {
+        sh "$SONAR_HOME/bin/sonar-scanner -Dsonar.projectName=${projectName} -Dsonar.projectKey=${projectKey}"
+    }
 }
