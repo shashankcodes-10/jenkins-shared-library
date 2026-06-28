3f95d6f95e92097195e1d63249e93faf296382a7
shashankcodes-10 <124920174+shashankcodes-10@users.noreply.github.com>
2026-06-29 00:00:10 +0530

    Update sonarqube_analysis.groovy

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -1,5 +1,10 @@
 def call(String sonarInstallation, String projectName, String projectKey) {
     withSonarQubeEnv(sonarInstallation) {
-        sh "$SONAR_HOME/bin/sonar-scanner -Dsonar.projectName=${projectName} -Dsonar.projectKey=${projectKey}"
+        sh """
+        $SONAR_HOME/bin/sonar-scanner \
+        -Dsonar.projectName=${projectName} \
+        -Dsonar.projectKey=${projectKey} \
+        -Dsonar.exclusions=trivy-fs-report.html,trivy-image-report.html,dependency-check-report.xml,mysql-data/**
+        """
     }
 }
